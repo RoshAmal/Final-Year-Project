043b1a0c1ce4a3104c9a4fec063cbf5451ce8582
vyshnavprasanth <vyshavprasanth2k@gmail.com>
2022-05-26 18:53:12 +0530

    temperature actual

--- TEMP/DHT11.ino
+++ TEMP/DHT11.ino
@@ -1,35 +1,106 @@
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
 
-#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
-#define DHTTYPE DHT11   // DHT 11
   
+// Set WiFi credentials
+const char *ssid = "Vyshnav";
+const char *pass = "123454321";
+unsigned int localPort = 3000; // local port to listen for UDP packets
 
 
-char ssid[] = "Vyshnav";     // replace with your wifi ssid and wpa2 key
-char pass[] = "12344321";
+IPAddress ClientIP(192,168,237,253);
+      
+#define DHTPIN 0
+#define DHTTYPE DHT11
 
+DHT dht(DHTPIN, DHTTYPE);
 
+float t = 0.0;
+float h = 0.0;
+char ipaddress[255];
+char packetBuffer[255];
+char replyBuffer[255] = "Hi there ";
+char macaddress[255];
 
-
-#define dht_dpin 0
-DHT dht(dht_dpin, DHTTYPE); 
-void setup(void)
-{ 
+WiFiUDP udp;
   
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
   dht.begin();
-  Serial.begin(9600);
-  Serial.println("Humidity and temperature\n\n");
-  delay(700);
-  
 
+  pinMode(D2 ,OUTPUT);
+  
+  // Begin WiFi
+  WiFi.begin(ssid, pass);
+  
+  
+  // Loop continuously while WiFi is not connected
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  // Connected to WiFi
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println(WiFi.localIP());
+  IPAddress ip = WiFi.localIP();
+ 
+        
+  //ipaddress=WiFi.localIP().toString().c_str();
+  sprintf(ipaddress, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+  
 }
+ 
 void loop() {
-    double h = dht.readHumidity();
-    float t = dht.readTemperature();         
-    Serial.print("Current humidity = ");
-    Serial.print(h);
-    Serial.print("%  ");
-    Serial.print("temperature = ");
-    Serial.print(t); 
-    Serial.println("C  ");
-  delay(800);
+
+  float newT = dht.readTemperature();
+    //Read temperature as Fahrenheit (isFahrenheit = true)
+   
+    if (isnan(newT))
+    {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else
+    {
+      t = newT;
+      Serial.println(t);
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+   
+    if (isnan(newH))
+    {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else
+   {
+      h = newH;
+      Serial.println(h);
+    }
+    
+    char temp[20];
+    char hum[20];
+
+    //function to conver to string
+    
+   sprintf(temp, "%f", t);//make the number into string using sprintf function
+   sprintf(hum, "%f", h);
+      
+     int cb = udp.parsePacket();
+      {
+        udp.beginPacket(ClientIP, 3000);
+        udp.write(ipaddress); //Send one byte to ESP8266
+        udp.write(temp);
+        udp.write(hum);
+        udp.endPacket();
+        delay(10000);
+      } 
+ 
 }
