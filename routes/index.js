1610cc0178cfe1632c92b9382594cf9b426d14fd
vyshnavprasanth <vyshavprasanth2k@gmail.com>
2022-05-26 17:34:09 +0530

    Temperature code added

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,30 @@ var express = require('express');
 var router = express.Router();
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-var temp,hum;
-
+var temp,hum,d1;
+var minutes,hour = 0;
 server.bind(3000);
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
 
   server.on('message',function(msg, rinfo){
-    temp = msg.slice(14, 18);
-    hum = msg.slice(25,29)
-     console.log("message recived is "+msg)
-     console.log("message recived is "+rinfo)
-   })
-   
+  d1 = new Date();  
+  temp = msg.slice(14, 16);
+  hum = msg.slice(23,25);
+  console.log("message recived is "+msg)
+  hour = d1.getHours();
+  minutes = d1.getMinutes();
+  });
 
-  
-  res.render('index', {temp,hum});
+   var indication="Am"
+   if (hour>12)
+   indication="Pm"
+
+   console.log("time"+hour)
+   res.render('index', {temp,hum,hour,minutes});
 });
+
 
 module.exports = router;
