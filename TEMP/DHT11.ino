db9e4178916b1cf3ccb6b6dc03c2550b1a38f485
vyshnavprasanth <vyshavprasanth2k@gmail.com>
2022-05-26 18:45:20 +0530

    temp added coreectly

---
+++ TEMP/DHT11.ino
@@ -0,0 +1,35 @@
+
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
+#define DHTTYPE DHT11   // DHT 11
+  
+
+
+char ssid[] = "Vyshnav";     // replace with your wifi ssid and wpa2 key
+char pass[] = "12344321";
+
+
+
+
+#define dht_dpin 0
+DHT dht(dht_dpin, DHTTYPE); 
+void setup(void)
+{ 
+  
+  dht.begin();
+  Serial.begin(9600);
+  Serial.println("Humidity and temperature\n\n");
+  delay(700);
+  
+
+}
+void loop() {
+    double h = dht.readHumidity();
+    float t = dht.readTemperature();         
+    Serial.print("Current humidity = ");
+    Serial.print(h);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(t); 
+    Serial.println("C  ");
+  delay(800);
+}
