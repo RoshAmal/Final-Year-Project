60008df48b6e0ef2a95493a962920d257cd97493
Amal Rosh <amalrosh707@gmail.com>
2022-05-28 19:21:10 +0530

    restructured

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+const { route } = require('express/lib/application');
 var server = dgram.createSocket('udp4');
 var temp,hum,d1;
 var minutes,hour = 0;
+import dgram from 'node:dgram';
+import { Buffer } from 'node:buffer';
+
+
+
 server.bind(3000);
+var list={tem:'192.168.0.100',
+led:'192.168.0.200',
+irs:'192.168.0.300',
+}
 
 /* GET home page. */
 
@@ -12,20 +23,30 @@ router.get('/', function(req, res, next) {
 
   server.on('message',function(msg, rinfo){
   d1 = new Date();  
-  temp = msg.slice(14, 16);
-  hum = msg.slice(23,25);
+  var key = msg.slice(15, 17);
+  dk[key]=msg.slice(0,14)
+  temp = msg.slice(18,20);
+  hum = msg.slice(22,24);
   console.log("message recived is "+msg)
   hour = d1.getHours();
   minutes = d1.getMinutes();
   });
 
-   var indication="Am"
-   if (hour>12)
-   indication="Pm"
-
-   console.log("time"+hour)
+   
    res.render('index', {temp,hum,hour,minutes});
 });
 
+router.post('/ledstatus', function(req, res, next) {
+  console.log(req.body.led)
+ 
+led= list['led']
+const message = Buffer.from(req.body.led);
+client.connect(1234, led, (err) => {
+  client.send(message, (err) => {
+    client.close();
+  });
+});
+  res.redirect('/')
+})
 
 module.exports = router;
