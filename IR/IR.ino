db5136f9d3852133b369b92f7318e0abe1da8736
Amal Rosh <amalrosh707@gmail.com>
2022-05-29 16:58:25 +0530

    ir done

--- IR/IR.ino
+++ IR/IR.ino
@@ -1,59 +1,53 @@
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-#include <Wire.h>
+ 
+  #define WIFI_SSID "Vyshnav"
+  int val =0;
+  const int IR =13; //D7 PIN
+  int select=0;
+  char ipaddress[255];
+  IPAddress ClientIP(192,168,1,202);  //specify the ip of server here.....
+  WiFiUDP UDP;
 
-
-//const char* ssid ="Anol";
-//const char* password = "10009000";
-const char* ssid ="Vyshnav";
-
-unsigned int localPort = 3000; // local port to listen for UDP packets
-int val=0;
-char cnt[10]="";
-WiFiUDP udp;
-IPAddress ClientIP(192,168,1,202); //to change
-
-
-
+  
 void setup() {
-  Serial.begin(9600); // sensor buart rate  
-  pinMode(14,HIGH);  // Led Pin Connected To D5 Pin  
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(IR,INPUT);
+  
 
   WiFi.mode(WIFI_STA);
-
-
-  WiFi.begin(ssid);
-
+  WiFi.begin(WIFI_SSID);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  Serial.println("");
+  Serial.println("NodeMCU is connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(3000);
+  IPAddress ip = WiFi.localIP();
+  //ipaddress=WiFi.localIP().toString().c_str();
+  sprintf(ipaddress, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(IR) == HIGH){
+    select = 1;
+    Serial.println("1");
+  }
+  else
+    Serial.println("0");//object detected by ir
 
+  //sendind ip ipaddres append with LED to server
 
-  cnt[0]='N';
-  val = digitalRead(5); // IR Sensor output pin connected to D1  
-  Serial.println(val);  // see the value in serial m0nitor in Arduino IDE  
-  delay(100);      // for timer  
-  if(val == 1 )  
-  {
-  
-    //send by udp to server
-    cnt[0]='Y';
-  
-  }  
-
-  int cb = udp.parsePacket();
+  int cb = UDP.parsePacket();
       {
-        udp.beginPacket(ClientIP, 3000);
-        udp.write("34"); //Send one byte to ESP8266
-        udp.write("IRS");
-        udp.endPacket();
+        UDP.beginPacket(ClientIP, 3000);
+        UDP.write(ipaddress); //Send ipaddress to server
+        UDP.write("irs");
+        UDP.endPacket();
         delay(1000);
       } 
 }
