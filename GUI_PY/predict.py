843d7319f3a9c276d50c90ee7f3e6eeb7bddb6ff
Amal Rosh <amalrosh707@gmail.com>
2022-06-04 11:15:46 +0530

    Prediction files

---
+++ GUI_PY/predict.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+from keras.models import load_model
+
+data = load_model("model.h5") 
+#lst=data["model"]
+
+df=pd.read_csv("DDOS1.csv")
+features = ['Fwd Pkt Len Std', 'Fwd Pkt Len Mean',
+       'Fwd Pkt Len Max', 'Fwd Seg Size Avg', 'Pkt Len Std', 'Flow IAT Std',
+       'Bwd Pkt Len Std', 'Bwd Seg Size Avg', 'Pkt Size Avg',
+       'Subflow Fwd Byts']
+X = df[features]
+X[features] = X[features].apply(pd.to_numeric, errors='coerce', axis=1)
+X = X.fillna(0)
+
+def show_predict():
+
+    st.title("DDoS Detection")
+
+    ok=st.button("Predict")
+    if ok:
+        result=data.predict(X)
+        if result>0.5:
+            prediction="Network Traffic is DDOS"
+        else:
+            prediction="Network Traffic is Benign"
+        st.subheader(prediction)
